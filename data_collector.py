292bade7737ce5237d76bde37fa0ee89f84ba5a8
fillerInk <thatonephysicsnerd@gmail.com>
2020-04-28 15:49:11 +0530

    Completed script

--- data_collector.py
+++ data_collector.py
@@ -1,59 +1,64 @@
 import re
 import requests
 import urllib3
+from os import system
 from bs4 import BeautifulSoup, SoupStrainer
 
 def collectSubLinks(url):
     page = requests.get(url)    
     data = page.text
-    soup = BeautifulSoup(data)
+    soup = BeautifulSoup(data,features='lxml')
 
     links = []
-
+    print('Collecting urls...')
     for link in soup.find_all('a'):
         links.append(link.get('href'))
         
     love_letter_links = []
     for i in range (21,111):
         love_letter_links.append(links[i])
-    
+    print('Final love letter links collected!')
     return love_letter_links
 
 def textCollect(love_letter_links):
+    print('Collecting content from links...')
+    content = []
     for i in range(len(love_letter_links)):
+        print('Collecting letter '+str(i))
+        print(' ')
         url = love_letter_links[i]
-
+        
         req = urllib3.PoolManager()
         res = req.request('GET',url)
         soup = BeautifulSoup(res.data,'html.parser')
 
-        content = soup.find(attrs={'class':'paracontent'}).find_all('p')
+        content.append(soup.find(attrs={'class':'paracontent'}).find_all('p'))
+    print('All text collected!')
     return content
 
 
 def textRangeSelector(content):
-    final_text = ''
-    
-    for i in range(3):
-        print(i)
-        print(content[i])
-        print()
+   final_text = ''
+   for i in range(3):
+       print(i)
+       for j in range(len(content[i])):
+           print(j)
+           print(content[i][j])
+           print()
+       print('Enter range to parse')
+       a = int(input('Enter lower limit'))
+       b = int(input('Enter one more than upper limit'))
 
-    print('Enter range to parse: ')
+       text = ''
 
-    a = input('Enter lower limit')
-    b = input('Enter one more than upper limit')
-
-    text = ''
-
-    for i in range(a,b):
-        text += str(content[i])
-    print(text)
-
-    soup = BeautifulSoup(text)
-    final_text += str(soup.get_text())
-
-    return final_text
+       for j in range(a,b):           
+           text += str(content[i][j])
+       
+       soup = BeautifulSoup(text)
+       final_text += str(soup.get_text())
+       final_text += "\n\n"
+       system('clear')
+   return final_text
 
 def main():
     url = "https://theromantic.com/LoveLetters/main.htm"
---
+++ hello.py
@@ -0,0 +1,7 @@
+from os import system
+
+a = input('Enter a number')
+print('The number you entered is ')
+print(a)
+system('clear')
+print("Screen cleared")
